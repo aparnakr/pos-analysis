12027a5dd30d6269ab03119df55b1ac57de2f940
ValarDragon <dojha12@gmail.com>
2018-01-05 23:35:43 -0600

    Minor Python speed improvements. (Memoization on factorials, improved conditionals)

--- polya.py
+++ polya.py
@@ -7,8 +7,19 @@ from numba import jit, njit, prange
 num_rounds = 3
 X_0 = [1000 for i in range(64)]
 num_validators = len(X_0)
+factorial_memo = {x:factorial(x) for x in range(0,num_rounds + 5)}
 
-# This calculates the multinomial i.e 
+# This calculates the multinomial i.e
+#  n!/ (y1! * y2! .. * ym!)
+def multinomial(arr_len, num, arr_denom):
+	if sum(arr_denom) != num:
+		return 0
+	denom = 1
+	for e in arr_denom:
+		denom *= factorial_memo[e]
+	return Decimal(factorial_memo[num])/Decimal(denom)
+
+# This calculates the multinomial i.e
 #  n!/ (y1! * y2! .. * ym!)
 def multinomial(arr_len, num, arr_denom):
 	if sum(arr_denom) != num:
@@ -18,13 +29,17 @@ def multinomial(arr_len, num, arr_denom):
 		denom *= factorial(e)
 	return Decimal(factorial(num))/Decimal(denom)
 
+rev_memo = {}
 # This calculates x * (x + 1) .. * (x + (n - 1))
 def rev_factorial(x, n):
+	if (x,n) in rev_memo:
+		return rev_memo[x,n]
 	i = 0
 	prod = 1
 	while i < n:
 		prod = prod * (x + i)
 		i += 1
+	rev_memo[(x,n)] = prod
 	return prod
 
 # This calculates prob(x_j = y_j + x_0, for j = 1.. m)
@@ -63,7 +78,7 @@ def multichoose(k, objects):
             j_1 = q
 
 
-# This returns all permutations of putting balls in the boxes 
+# This returns all permutations of putting balls in the boxes
 def all_sets(boxes, balls):
 	# rng = list(range(balls + 1)) * boxes
 	# return set(i for i in permutations(rng, boxes) if sum(i) == balls)
@@ -83,16 +98,17 @@ def all_sets(boxes, balls):
 def f(X, s, j):
 	return X[j] == s
 
-# Returns the probability of the i^th validator having exactly p 
-# proportion of the stake 
-# remeber to input p in form of 4.0/5 
+# Returns the probability of the i^th validator having exactly p
+# proportion of the stake
+# remeber to input p in form of 4.0/5
 def i_exact_stake(X_0, p, n, m, i):
 	t_n = sum(X_0) + n
 	s = t_n * p
 	prob = 0
 	for Y in all_sets(m, n):
 		y_x = [Y[j] + X_0[j] for j in range(m)]
-		prob += f(y_x, s, i)* prob_win_vector(n, m, Y, X_0)
+		if f(y_x, s, i):
+			prob += prob_win_vector(n, m, Y, X_0)
 	return prob
 
 # Helper function for prob_any_threshold such that it returns true
@@ -103,9 +119,9 @@ def g(X, s):
 			return True
 	return False
 
-# Returns the probability of the any validator having >= p 
-# proportion of the stake 
-# remeber to input p in form of 4.0/5 
+# Returns the probability of the any validator having >= p
+# proportion of the stake
+# remeber to input p in form of 4.0/5
 def prob_any_threshold(X_0, p, n, m):
 	t_n = sum(X_0) + n
 	s = t_n * p
@@ -113,11 +129,12 @@ def prob_any_threshold(X_0, p, n, m):
 	sets = all_sets(m, n)
 	for Y in sets:
 		y_x = [Y[j] + X_0[j] for j in range(m)]
-		prob += g(y_x, s)* prob_win_vector(n, m, Y, X_0)
+		if g(y_x, s):
+			prob += prob_win_vector(n, m, Y, X_0)
 	return prob
 
 # Returns the expected stake composition vector of the validators
-# after n rounds. 
+# after n rounds.
 def expected_value(X_0, n, m):
 	expected = X_0
 	for Y in all_sets(m, n):
@@ -125,7 +142,7 @@ def expected_value(X_0, n, m):
 		expected = [expected[i] + Y_i[i] for i in range(m)]
 	return expected
 
-# check to see that the probabilities are written out right. 
+# check to see that the probabilities are written out right.
 # for Y in all_sets(3,4):
 # 	#print(Y, prob_win_vector(4, 3, Y, [1, 2, 1]))
 # 	prob+= prob_win_vector(4, 3, Y, [1, 2, 1])
